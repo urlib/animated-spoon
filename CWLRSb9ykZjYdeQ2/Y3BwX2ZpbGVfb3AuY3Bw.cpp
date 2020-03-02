5033f3fa775e738350557207dce8c81f61fe0dbe
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-02 16:19:56 +0800

    Original path: C:/Users/joefa/Desktop/WorkSpace/lab/cpp_file_op.cpp

---
+++ CWLRSb9ykZjYdeQ2/Y3BwX2ZpbGVfb3AuY3Bw.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main()
+{
+    ifstream input;
+    input.open("std.in");
+    ofstream output;
+    output.open("std.out");
+    input.sync_with_stdio(0);
+    output.sync_with_stdio(0);
+    input.tie(0);
+    output.tie(0);
+    return 0;
+}
